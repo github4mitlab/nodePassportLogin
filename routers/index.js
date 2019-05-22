26dd81de810a45423868c1547ea65f75b07ba4bd
surfaceMay <may1516@gmail.com>
2019-05-22 20:30:36 +0900

    welcome page

--- routers/index.js
+++ routers/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send("Welcome");
+    //console.log("hi");
+    res.render("welcome");
 });
 
 
 module.exports = router;
+
