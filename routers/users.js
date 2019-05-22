7d2cf9f78f94b4729845be62a84e25015fdb9c97
surfaceMay <may1516@gmail.com>
2019-05-22 21:11:39 +0900

    register login dashboard

--- routers/users.js
+++ routers/users.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/login", (req, res) => {
-    res.send("Login");
+    res.render("Login");
 });
 
 router.get("/register", (req, res) => {
-    res.send("Register");
+    res.render("register");
 });
 
 
