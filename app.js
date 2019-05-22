6024f1dc2bb5ca1eccddb088b37f0e513590f044
surfaceMay <may1516@gmail.com>
2019-05-22 20:03:24 +0900

    Add views files

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const app = express();
+
+// EJS
+app.use(expressLayouts);
+app.set("view engine", "ejs"); // 매뉴얼에 이렇게 하도록 미리 세팅이 되어 있음 암기
+
+
 
 app.use("/", require("./routers/index"));
 app.use("/users", require("./routers/users"));
+
 
 
 const PORT = process.env.PORT || 5000;
